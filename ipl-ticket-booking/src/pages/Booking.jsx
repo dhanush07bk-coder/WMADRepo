797f9d29c88cc7834fbd9e24bb441a884df2e0bf
Dhanush <Dhanush07bk@gmail.com>
2026-09-22 22:55:21 +0530

    Add IPL ticket booking app

---
+++ ipl-ticket-booking/src/pages/Booking.jsx
@@ -0,0 +1,7 @@
+function Booking() {
+  return (
+    <h1>Book Your Ticket</h1>
+  );
+}
+
+export default Booking;
---
+++ ipl-ticket-booking/src/pages/BookingHistory.jsx
@@ -0,0 +1,6 @@
+function BookingHistory() {
+  return (
+    <h1>Booking History</h1>
+  );
+}
+export default BookingHistory;
